83dd27a47cb97c3b9b84a65b2a623825b7c86f7e
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-01-10 00:02:36 +0100

    chore(umd): change umd wrapper configuration

--- tasks/umd.js
+++ tasks/umd.js
@@ -1,15 +1,18 @@
-module.exports = {
-  lib: {
-    template: 'umd',
-    indent: '  ',
-    src: 'dist/<%= pkg.name.replace(/.js$/, "") %>.js',
-    dest: 'dist/<%= pkg.name.replace(/.js$/, "") %>.js',
-    returnExportsGlobal: 'container',
-    deps: {
-      default: [],
-      amd: [],
-      cjs: [],
-      global: []
+module.exports = function () {
+  'use strict';
+  return {
+    lib: {
+      template: require('path').join(process.cwd(), './tasks/templates/umd.hbs'),
+      indent: '  ',
+      src: 'dist/<%= pkg.name.replace(/.js$/, "") %>.js',
+      dest: 'dist/<%= pkg.name.replace(/.js$/, "") %>.js',
+      objectToExport: 'Container',
+      deps: {
+        default: [],
+        amd: [],
+        cjs: [],
+        global: []
+      }
     }
-  }
+  };
 };
